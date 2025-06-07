de586b3b41993b4472718a8cebaac29aa96b2502
Saurabh Maurya <srbmaury@gmail.com>
2025-06-07 06:52:41 +0000

    added remote frontend url in app.js

--- server/app.js
+++ server/app.js
@@ -4,11 +4,24 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://store-management-frontend-kq05.onrender.com/'
+];
+
+app.use(cors({
+	origin: (origin, callback) => {
+		if (!origin || allowedOrigins.includes(origin)) {
+			callback(null, true);
+		} else {
+			callback(new Error('Not allowed by CORS'));
+		}
+	},
+	credentials: true
+})); app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('API is running...');
+	res.send('API is running...');
 });
 
 const authRoutes = require('./routes/authRoutes');
