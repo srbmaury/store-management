32faa40fafe3aeb5f8d0628ea303abdb9b15be90
Saurabh Maurya <srbmaury@gmail.com>
2025-06-07 15:05:38 +0000

    back to render

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
 	plugins: [react()],
 	server: {
 		proxy: {
-			'/api': 'https://store-management-js3x.onrender.com/',
+			'/api': 'http://localhost:5000',
 		},
 	},
 	test: {
--- server/app.js
+++ server/app.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 
-app.use(cors()); 
+app.use(cors({
+	origin: 'https://store-management-copy-frontend.onrender.com', // your frontend URL
+}));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-	res.send('API is running...');
-});
+// -------------------------- DEPLOYMENT ---------------------------
+const __dirname1 = path.resolve();
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname1, "client", "dist"))); // Vite builds to dist
+
+	// SPA fallback route
+	app.get('/{*any}', (req, res) => {
+		res.sendFile(path.join(__dirname1, 'client', 'dist', 'index.html'));
+	});
+} else {
+	app.get("/", (req, res) => {
+		res.send("API is running successfully");
+	});
+}
+// -------------------------- DEPLOYMENT ---------------------------
+
+// Routes
 const authRoutes = require('./routes/authRoutes');
 const inventoryRoutes = require('./routes/inventoryRoutes');
 const salesRoutes = require('./routes/salesRoutes');
