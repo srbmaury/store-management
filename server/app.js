e76b651b739431e78be9762719981c3a8f0068ea
Saurabh Maurya <srbmaury@gmail.com>
2025-06-07 14:18:48 +0530

    added changes for deploying to vercel (#11)

--- server/app.js
+++ server/app.js
@@ -4,21 +4,8 @@ require('dotenv').config();
 
 const app = express();
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://store-management-frontend-kq05.onrender.com/'
-];
-
-app.use(cors({
-	origin: (origin, callback) => {
-		if (!origin || allowedOrigins.includes(origin)) {
-			callback(null, true);
-		} else {
-			callback(new Error('Not allowed by CORS'));
-		}
-	},
-	credentials: true
-})); app.use(express.json());
+app.use(cors()); 
+app.use(express.json());
 
 app.get('/', (req, res) => {
 	res.send('API is running...');
